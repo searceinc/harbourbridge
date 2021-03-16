a2ace22dfc6deb09efa45015188aeb3993c8935b
Ashit Rai <ashitrai@administrators-MacBook-Air-8.local>
2021-03-16 17:53:54 +0530

    added jest for testing application

--- frontend/app/view/instructions/instructions.js
+++ frontend/app/view/instructions/instructions.js
@@ -8,3 +8,9 @@ const renderInstructionsHtml = () => {
   $(".modal-backdrop").hide();
   jQuery('#app').load('./app/view/instructions/instructions.html');
 }
+
+const funnctions = {
+  add:(num1, num2) => num1 + num2
+}
+
+module.exports = funnctions;
