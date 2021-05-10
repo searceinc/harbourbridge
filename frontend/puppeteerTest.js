d91ef8f71be051c05308255e85edb37b911c3429
searce-anubhav <anubhav.chakraborty@searce.com>
2021-05-10 17:00:45 +0530

    methods and description for puppeteer

--- frontend/puppeteerTest.js
+++ frontend/puppeteerTest.js
@@ -1,21 +1,5 @@
 const puppeteer = require("puppeteer");
 
-let arr = [];
-for (let i = 1; i <= 14; i++) {
-  arr.push("select#data-type-option" + i);
-}
-
-const map = async (page, arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    await page.waitForSelector(arr[i]);
-    await page.select(arr[i], "STRING");
-  }
-};
-
-const printInstructions = async (msg) => {
-  await console.log(msg);
-};
-
 let config = {
   launchOptions: {
     headless: false,
@@ -23,69 +7,89 @@ let config = {
   },
 };
 
-const passed = async (num) => {
-  console.log("Test Passed: ", num);
-};
-
-const homePage = {
-  loadDatabase: 'div[data-target="#loadDatabaseDumpModal"]',
-  selectMenu: "select#load-db-type",
-  dbFileName: "input#dump-file-path",
-  confirmButton: "input#load-connect-button",
-  categoryTable: "a#id-report-2",
-  categoryEditSchemaButton: "button#editSpanner2",
-  categoryNameInput: "input#column-name-text-211",
-  categorySelectMenu: "select#data-type-211",
-  categoryConstraint: "div#btn-group-#sp-constraint-00",
-  paymentTable: "a#id-report-12",
-  paymentEditSchemaButton: "button#editSpanner12",
-  paymentRentalInput: "input#column-name-text-1233",
-  errorModalButton: "input#edit-table-warning",
-  paymentDropRentalButton: "button#payment0foreignKey",
-  dropModalConfirmButton: "input#fk-drop-confirm",
-  actorsTable: "a#id-report-0",
-  actorsAddIndexButton: "button#hb-0indexButton-actor",
-  addIndexModalInput: "input#index-name",
-  addIndexKeyCheckbox: "span#index-checkbox-first_name-1",
-  createIndexButton: "input#create-index-button",
-  actorsEditSchemaButton: "button#editSpanner0",
-  actorsInputField: "input#column-name-text-011",
-  actorsDropButton: "button#actor1secIndex",
-  customerTable: "a#id-report-5",
-  customerEditSchemaButton: "button#editSpanner5",
-  customerDropFK: "button#customer0foreignKey",
-  customerConfirmDropFK: "input#fk-drop-confirm",
-  customerDropSK: "button#customer0secIndex",
-  customerConfirmDropSK: "input#fk-drop-confirm",
-  downloadSessionFile: "button#download-schema",
-  homeScreen: "a#homeScreen",
-  loadSessionFile: 'div[data-target="#loadSchemaModal"]',
-  importDbType: "select#import-db-type",
-  sessionFilePath: "input#session-file-path",
-  pathOfSessionFile: "/Users/anubhavchakraborty/Downloads/session.json", // PATH IS DIFFERENT FOR DIFFERENT SYSTEMS
-  loadSessionButton: "input#load-session-button",
-  resumeSession: "a#session0",
-  editGlobalDataType: "button#editButton",
-  selectDataTypeOption: "select#data-type-option",
-  confirmDataType: "input#data-type-button",
-  assertChanges: "div#save-data-type-00",
-  ddlStatementTab: "a#ddlTab",
-  openFirstTable: "a#id-ddl-0",
-  searchInput: "input#search-input",
-  ddlStatementTab: "a#ddlTab",
-  downloadDDLStatement: "button#download-ddl",
-  summaryReport: "a#summaryTab",
-  downloadSummaryReport: "button#download-report",
-};
-
 puppeteer.launch(config.launchOptions).then(async (browser) => {
   const page = await browser.newPage();
   await page.goto("http://localhost:8080/");
 
-  // await page.setUserAgent(
-  //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-  // );
+  await runNavigation(page);
 
+  /*
+  Pick any column name which is not dependent on any 
+  foreign key or index and change its name and data type
+  */
+  await runTest1(page);
+
+  //Change the name of secondary index
+  await runTest2(page);
+
+  /*
+  Pick any column which is part of foreign key or index 
+  and try changing its name, data type and constraint. 
+  Resolve the dependencies and show the changes
+  */
+  await runTest3(page);
+
+  //Delete secondary index and foreign key
+  await runTest4(page);
+
+  /*
+  Create a new secondary index and try to edit column 
+  which is acting as key in this sec index. It should 
+  give error, then delete the index and try to edit 
+  column again , now it should work
+  */
+  await runTest5(page);
+
+  /*
+  Try to create a sec index, when there are pending 
+  changes in the table and show the error message
+  */
+  await runTest6(page);
+
+  /*
+  Change column name, rename foreign key, rename 
+  index all at once and then try to save the changes
+  */
+  await runTest7(page);
+
+  /*
+  Try giving duplicate names in foreign key, index and change 
+  column name as well and then resolve all the errors one by one
+  */
+  await runTest8(page);
+
+  /*
+  Download session file and try using 
+  load session modal from home screen
+  */
+  await runTest9(page);
+
+  /*
+  Use global edit data type and convert everything into 
+  string to show the changes in every table
+  */
+  await runTest10(page);
+
+  /*
+  For every change (like data type or column change ) show the 
+  respective changes in view summary, ddl statements and summary tab as well
+  */
+  await runTest11(page);
+
+  /*
+  Cover all basic functionalities like search 
+  table, downloading ddl, summary reports etc
+  */
+  await runTest12(page);
+
+  //Click on resume sessions on home screen to show the functionality
+  await runTest13(page);
+
+  // Close Browser
+  await browser.close();
+});
+
+const runNavigation = async (page) => {
   await page.waitForTimeout(200);
 
   await page.waitForSelector(homePage.loadDatabase);
@@ -107,9 +111,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.click(homePage.confirmButton);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 1
-
+const runTest1 = async (page) => {
   await page.waitForSelector(homePage.categoryTable);
   await page.click(homePage.categoryTable);
 
@@ -131,9 +135,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(1);
+};
 
-  // Test 2
-
+const runTest2 = async (page) => {
   await page.waitForSelector(homePage.paymentTable);
   await page.click(homePage.paymentTable);
 
@@ -153,12 +157,12 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForSelector(homePage.paymentEditSchemaButton);
   await page.click(homePage.paymentEditSchemaButton);
 
-  await passed(2);
-
   await page.waitForTimeout(200);
 
-  // Test 3
+  await passed(2);
+};
 
+const runTest3 = async (page) => {
   await page.waitForSelector(homePage.paymentEditSchemaButton);
   await page.click(homePage.paymentEditSchemaButton);
 
@@ -198,9 +202,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(3);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 4
-
+const runTest4 = async (page) => {
   await page.waitForSelector(homePage.customerTable);
   await page.click(homePage.customerTable);
 
@@ -237,9 +241,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(4);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 5
-
+const runTest5 = async (page) => {
   await page.waitForSelector(homePage.actorsTable);
   await page.click(homePage.actorsTable);
 
@@ -302,9 +306,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(5);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 6
-
+const runTest6 = async (page) => {
   await page.waitForSelector(homePage.actorsEditSchemaButton);
   await page.click(homePage.actorsEditSchemaButton);
 
@@ -333,9 +337,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(6);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 7
-
+const runTest7 = async (page) => {
   await page.waitForSelector(homePage.paymentEditSchemaButton);
   await page.click(homePage.paymentEditSchemaButton);
 
@@ -361,9 +365,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(7);
 
   await page.waitForTimeout(500);
+};
 
-  // Test 8
-
+const runTest8 = async (page) => {
   await page.waitForSelector(homePage.paymentEditSchemaButton);
   await page.click(homePage.paymentEditSchemaButton);
 
@@ -410,9 +414,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await passed(8);
 
   await page.waitForTimeout(200);
+};
 
-  // Test 9
-
+const runTest9 = async (page) => {
   await page.waitForSelector(homePage.downloadSessionFile);
   await page.click(homePage.downloadSessionFile);
 
@@ -444,9 +448,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(9);
+};
 
-  // Test 10
-
+const runTest10 = async (page) => {
   await page.waitForSelector(homePage.editGlobalDataType);
   await page.click(homePage.editGlobalDataType);
 
@@ -477,9 +481,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(10);
+};
 
-  // Test 11
-
+const runTest11 = async (page) => {
   await page.waitForSelector(homePage.ddlStatementTab);
   await page.click(homePage.ddlStatementTab);
 
@@ -507,9 +511,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(11);
+};
 
-  // Test 12
-
+const runTest12 = async (page) => {
   await page.waitForSelector(homePage.searchInput);
   await page.type(homePage.searchInput, "store");
 
@@ -534,9 +538,9 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(12);
+};
 
-  // Test 13
-
+const runTest13 = async (page) => {
   await page.click(homePage.homeScreen);
 
   await page.waitForTimeout(200);
@@ -547,7 +551,75 @@ puppeteer.launch(config.launchOptions).then(async (browser) => {
   await page.waitForTimeout(200);
 
   await passed(13);
+};
 
-  //Browser Close
-  await browser.close();
-});
+let arr = [];
+for (let i = 1; i <= 14; i++) {
+  arr.push("select#data-type-option" + i);
+}
+
+const map = async (page, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    await page.waitForSelector(arr[i]);
+    await page.select(arr[i], "STRING");
+  }
+};
+
+const printInstructions = async (msg) => {
+  await console.log(msg);
+};
+
+const passed = async (num) => {
+  console.log("Test Passed: ", num);
+};
+
+const homePage = {
+  loadDatabase: 'div[data-target="#loadDatabaseDumpModal"]',
+  selectMenu: "select#load-db-type",
+  dbFileName: "input#dump-file-path",
+  confirmButton: "input#load-connect-button",
+  categoryTable: "a#id-report-2",
+  categoryEditSchemaButton: "button#editSpanner2",
+  categoryNameInput: "input#column-name-text-211",
+  categorySelectMenu: "select#data-type-211",
+  categoryConstraint: "div#btn-group-#sp-constraint-00",
+  paymentTable: "a#id-report-12",
+  paymentEditSchemaButton: "button#editSpanner12",
+  paymentRentalInput: "input#column-name-text-1233",
+  errorModalButton: "input#edit-table-warning",
+  paymentDropRentalButton: "button#payment0foreignKey",
+  dropModalConfirmButton: "input#fk-drop-confirm",
+  actorsTable: "a#id-report-0",
+  actorsAddIndexButton: "button#hb-0indexButton-actor",
+  addIndexModalInput: "input#index-name",
+  addIndexKeyCheckbox: "span#index-checkbox-first_name-1",
+  createIndexButton: "input#create-index-button",
+  actorsEditSchemaButton: "button#editSpanner0",
+  actorsInputField: "input#column-name-text-011",
+  actorsDropButton: "button#actor1secIndex",
+  customerTable: "a#id-report-5",
+  customerEditSchemaButton: "button#editSpanner5",
+  customerDropFK: "button#customer0foreignKey",
+  customerConfirmDropFK: "input#fk-drop-confirm",
+  customerDropSK: "button#customer0secIndex",
+  customerConfirmDropSK: "input#fk-drop-confirm",
+  downloadSessionFile: "button#download-schema",
+  homeScreen: "a#homeScreen",
+  loadSessionFile: 'div[data-target="#loadSchemaModal"]',
+  importDbType: "select#import-db-type",
+  sessionFilePath: "input#session-file-path",
+  pathOfSessionFile: "/Users/anubhavchakraborty/Downloads/session.json", // PATH IS DIFFERENT FOR DIFFERENT SYSTEMS
+  loadSessionButton: "input#load-session-button",
+  resumeSession: "a#session0",
+  editGlobalDataType: "button#editButton",
+  selectDataTypeOption: "select#data-type-option",
+  confirmDataType: "input#data-type-button",
+  assertChanges: "div#save-data-type-00",
+  ddlStatementTab: "a#ddlTab",
+  openFirstTable: "a#id-ddl-0",
+  searchInput: "input#search-input",
+  ddlStatementTab: "a#ddlTab",
+  downloadDDLStatement: "button#download-ddl",
+  summaryReport: "a#summaryTab",
+  downloadSummaryReport: "button#download-report",
+};
